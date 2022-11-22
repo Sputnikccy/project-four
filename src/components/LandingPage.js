28e54007cea39722c61ea1624c0b2f7a4f7aac2e
Caiyi <plutoccy@gmail.com>
2022-11-21 22:48:04 -0700

    ok

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,7 +6,7 @@ const LandingPage = () => {
              <h1>Flock of friends!</h1>
 
             <div className='subtitle'>
-                <p>Trouble wrangling your friends to get together to do an activity?</p>
+                <p>11rouble wrangling your friends to get together to do an activity?</p>
                 <p>Have no idea what to do on weekends?</p>
                 <p>Check out hundreds of ways to have fun with friends! </p>
             </div>
